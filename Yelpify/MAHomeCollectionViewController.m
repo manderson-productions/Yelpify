f34d48f457de8e3d26439fd805a5d422d9983198
manderson-productions <markmakingmusic@gmail.com>
2016-02-22 18:35:51 -0800

    Finishing caching feature for home vc + handling network failures.

--- Yelpify/MAHomeCollectionViewController.m
+++ Yelpify/MAHomeCollectionViewController.m
@@ -10,6 +10,7 @@
 #import "MANetworkManager.h"
 #import "MABusinessCollectionViewCell.h"
 #import "Yelpify-Swift.h"
+#import "MAImageCache.h"
 
 static NSString *const kCollectionViewCellReuseID = @"CollectionViewCellReuseID";
 static NSUInteger const kNumberOfSectionsInCollectionView = 1;
@@ -18,7 +19,7 @@ static CGFloat const kNumCellsPerRow = 3.0;
 
 @interface MAHomeCollectionViewController ()
 
-@property (strong, nonatomic) NSMutableArray *imageCache;
+@property (strong, nonatomic) MAImageCache *imageCache;
 @property (assign, nonatomic) BOOL isRefreshing;
 
 @end
@@ -35,37 +36,67 @@ static CGFloat const kNumCellsPerRow = 3.0;
 #pragma mark - Setup Methods
 
 - (void)setupCache {
-    self.imageCache = [NSMutableArray array];
+    self.imageCache = [MAImageCache unarchive];
 }
 
 #pragma mark - Custom Methods
 
 - (void)requestBusinesses {
+    self.isRefreshing = YES;
+    NSLog(@"Started Refreshing");
     __weak typeof(self)weakSelf = self;
     [MAActivityIndicatorView show];
+
     [MANetworkManager searchWithOffset:self.imageCache.count completionBlock:^(NSArray *results, NSError *error) {
         __strong typeof(weakSelf)strongSelf = weakSelf;
-        if (strongSelf) {
+        if (!strongSelf) { return; }
 
-            dispatch_group_t dispatchGroup = dispatch_group_create();
-
-            for (NSDictionary *result in results) {
-                dispatch_group_enter(dispatchGroup);
-                __block NSURL *imageURL = [NSURL URLWithString:result[@"image_url"]];
-                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                    NSPurgeableData *data = [NSPurgeableData dataWithContentsOfURL:imageURL];
-
-                    // TODO: Not threadsafe, put on custom serial queue!!!
-                    [strongSelf.imageCache addObject:[UIImage imageWithData:data]];
-                    dispatch_group_leave(dispatchGroup);
-                });
-            }
-
-            dispatch_group_notify(dispatchGroup, dispatch_get_main_queue(), ^{
-                strongSelf.isRefreshing = NO;
-                [MAActivityIndicatorView hide];
+        void (^cleanup)(BOOL) = ^(BOOL doReloadData) {
+            [MAActivityIndicatorView hide];
+            NSLog(@"Stopped Refreshing");
+            if (doReloadData) {
                 [strongSelf.collectionView reloadData];
+            }
+            strongSelf.isRefreshing = NO;
+        };
+
+        void (^unexpectedBehavior)(NSString *) = ^(NSString *message) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [strongSelf showAlertControllerWithMessage:message withCompletionBlock:^{
+                    cleanup(NO);
+                }];
             });
+        };
+
+        // Some unexpected behavior occurred; handle it appropriately
+        NSString *unexpectedBehaviorMessage = nil;
+        if (error) {
+            unexpectedBehaviorMessage = error.localizedDescription;
+        } else if (!results.count) {
+            unexpectedBehaviorMessage = @"We could not find any businesses in your location. Please try again later.";
+        }
+
+        // Great success; Filter the results and cache the images from the results
+        if (!unexpectedBehaviorMessage) {
+            [strongSelf cacheResults:results withCompletionBlock:^{
+                cleanup(YES);
+            }];
+        } else {
+            unexpectedBehavior(unexpectedBehaviorMessage);
+        }
+    }];
+}
+
+#pragma mark - Helper Methods
+
+- (void)cacheResults:(NSArray *)results withCompletionBlock:(void(^)(void))completionBlock {
+    __block NSMutableArray *imageURLs = [NSMutableArray arrayWithCapacity:results.count];
+    [results enumerateObjectsUsingBlock:^(NSDictionary * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        [imageURLs addObject:[NSURL URLWithString:obj[@"image_url"]]];
+    }];
+    [self.imageCache cacheImagesFromURLStrings:imageURLs withCompletionBlock:^{
+        if (completionBlock) {
+            dispatch_async(dispatch_get_main_queue(), completionBlock);
         }
     }];
 }
@@ -76,13 +107,43 @@ static CGFloat const kNumCellsPerRow = 3.0;
             scrollView.contentSize.height + kPullToRefreshOffset;
 }
 
+- (void)showAlertControllerWithMessage:(NSString *)message withCompletionBlock:(void(^)(void))completionBlock {
+    UIAlertController *controller = [UIAlertController alertControllerWithTitle:@"Yelpify"
+                                                                         message:message
+                                                                  preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"OK"
+                                                           style:UIAlertActionStyleCancel
+                                                         handler:^(UIAlertAction * _Nonnull action) {
+                                                             if (completionBlock) {
+                                                                 completionBlock();
+                                                             }
+                                                         }];
+    __weak typeof(self)weakSelf = self;
+    UIAlertAction *tryAgainAction = [UIAlertAction actionWithTitle:@"Try Again" style:UIAlertActionStyleDefault
+                                                          handler:^(UIAlertAction * _Nonnull action) {
+                                                              __strong typeof(weakSelf)strongSelf = weakSelf;
+                                                              if (completionBlock) {
+                                                                  completionBlock();
+                                                              }
+                                                              if (strongSelf) {
+                                                                  [strongSelf requestBusinesses];
+                                                              }
+                                                          }];
+    [controller addAction:cancelAction];
+    [controller addAction:tryAgainAction];
+    [self presentViewController:controller animated:YES completion:nil];
+}
+
 #pragma mark - CollectionView Delegate / Datasource
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
 
     MABusinessCollectionViewCell *cell = (MABusinessCollectionViewCell *)[collectionView dequeueReusableCellWithReuseIdentifier:kCollectionViewCellReuseID forIndexPath:indexPath];
 
-    [cell setBusinessImage:self.imageCache[indexPath.row]];
+    UIImage *image = [self.imageCache imageAtIndex:indexPath.row];
+    if (image) {
+        [cell setBusinessImage:image];
+    }
 
     return cell;
 }
@@ -103,9 +164,10 @@ static CGFloat const kNumCellsPerRow = 3.0;
     return CGSizeMake(widthHeight, widthHeight);
 }
 
+#pragma mark - UIScrollviewDelegate
+
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
     if ([self shouldRefresh:scrollView]) {
-        self.isRefreshing = YES;
         [self requestBusinesses];
     }
 }
